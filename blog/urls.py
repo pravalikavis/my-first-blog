a0c2894b623900fbe6b3736f8dab9dd5500d1f06
pravalika <pravalikavis@gmail.com>
2018-03-09 21:54:47 +0530

    url changed

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from django.test import TestCase
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
 ]
